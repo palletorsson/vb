a60110c3a86525fb177f266ba830d0674c28ee48
palle torsson <palle.torsson@gmail.com>
2012-10-12 16:23:56 +0200

    adding variation model

--- vamlingbolaget/products/models.py
+++ vamlingbolaget/products/models.py
@@ -4,11 +4,18 @@ from django.db import models
 Kattens Modeller
 """
 
+class Variation(models.Model):
+    name = models.CharField(max_length=40)
+    color = models.ForeignKey('Color')
+    pattern = models.ForeignKey('Pattern')    
+    file = models.ImageField("Image", upload_to="variations")
+    active = models.BooleanField("Active")
+
 class Size(models.Model):
     """
     Size used in Product Model         
     """
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=10)
     
     def __unicode__(self):
         return unicode(self.name)
@@ -18,7 +25,10 @@ class Color(models.Model):
     """
     Color used in Product Model         
     """
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=20)
+    quality = models.ForeignKey('Quality')
+    file = models.ImageField("Image", upload_to="colors")
+    active = models.BooleanField("Active")
     
     def __unicode__(self):
         return unicode(self.name)
@@ -28,7 +38,10 @@ class Pattern(models.Model):
     """
     Pattern used in Product Model         
     """
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=20)
+    quality = models.ForeignKey('Quality')
+    file = models.ImageField("Image", upload_to="patterns")
+    active = models.BooleanField("Active")
     
     def __unicode__(self):
         return unicode(self.name)
@@ -39,6 +52,7 @@ class Quality(models.Model):
     Quality used in Article Model         
     """
     name = models.CharField(max_length=100)
+    active = models.BooleanField("Active")
 
     def __unicode__(self):
         return unicode(self.name)
@@ -49,7 +63,9 @@ class Type(models.Model):
     Type used in Article Model         
     """
     name = models.CharField(max_length=100)
-
+    quality = models.ForeignKey('Quality')
+    active = models.BooleanField("Active")
+    
     def __unicode__(self):
         return unicode(self.name)
 
@@ -63,7 +79,7 @@ class Article(models.Model):
     quality = models.ForeignKey('Quality')
     type = models.ForeignKey('Type')
     price = models.IntegerField()
-    file = models.ImageField("Image", upload_to="article")
+    file = models.ImageField("Image", upload_to="articles")
     active = models.BooleanField("Active")
     
     def __unicode__(self):
