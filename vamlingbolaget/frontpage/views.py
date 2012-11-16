f07e3ffcbcd6457843b2b8af15a2979b00023014
Katarina <katarina@katarina-VGN-CR35S-L.(none)>
2012-11-16 15:56:28 +0100

    added new frontpage style and vb.js

--- vamlingbolaget/frontpage/views.py
+++ vamlingbolaget/frontpage/views.py
@@ -2,14 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from models import Frontpage
+from gallery.models import Gallery
 from blog.models import Post
 
 
 def first_page(request):
     frontpage = Frontpage.objects.get(pk=1)
+    gallery = Gallery.objects.get(name = 'Frontpage')
+    images = gallery.image_set.all()
+    print '--------------------------------------'
+    print images
     news = Post.objects.all()
 
     return render_to_response('frontpage/first_page.html',
         {'frontpage': frontpage,
+         'images': images,
          'news': news,},
         context_instance=RequestContext(request))
--- vamlingbolaget/theme/static/js/vb.js
+++ vamlingbolaget/theme/static/js/vb.js
@@ -16,7 +16,8 @@ CartItemCollection = Backbone.Tastypie.Collection.extend({
 */
 
 $('document').ready(function(){
-    var getsrc = function(me, from, to){
+
+    var changesrc = function(me, from, to){
         var oldsrc = me.attr('src'),
             newsrc = oldsrc.replace(from,to);
         return newsrc;
@@ -24,32 +25,54 @@ $('document').ready(function(){
 
     var initgallery = function(){
         $('.enlarge').hover(function(){
-             $(this).attr('src', getsrc($(this), 'thumbnail','small'))
+             $(this).attr('src', changesrc($(this), 'thumbnail','small'))
         }, function(){
-             $(this).attr('src', getsrc($(this), 'small', 'thumbnail'))
+             $(this).attr('src', changesrc($(this), 'small', 'thumbnail'))
         });
     
         $('.isfeatured').hover(function(){
-             $(this).attr('src', getsrc($(this), 'medium','large'))
+             $(this).attr('src', changesrc($(this), 'medium','medium'))
         }, function(){
-             $(this).attr('src', getsrc($(this), 'large','thumbnail'))
+             $(this).attr('src', changesrc($(this), 'medium','thumbnail'))
              $(this).removeClass('isfeatured').addClass('enlarge');
              initgallery();
         }); 
+        
     }
     initgallery();
 
-    $('.overlay').each(function(){
-        console.log($(this));
+    /*
+     * Overlay for gallery feature images that
+     * dynamically calculates size of image.
+     * Tthe siblings are the feature images, perform check when these are loaded
+     */
+    
+    $('.overlay').each(function(){ 
         var me = $(this);
-        $(this).siblings().load(function(){
+        $(this).siblings().load(function(){ 
             var width = $(this).css('width'),
                 pos = $(this).offset();
-                
             me.css({'width': width, 'left':pos.left, 'top':pos.top, 'height': '20px', 'background-color':'#000', 'color':'#fff'});
-            console.log($(this).css('width'));
         })
-        
     });
+    
+    /*
+     * First-page spans whole screen, set the div-width and height dynamically based on viewport-width
+     * and adjust the image heights accordingly, remove space
+     */
+    
+    $('.spanfullscreen').each(function(){ 
+        var wheight = $(window).height(),
+            fullheight = wheight-130,
+            img = $(this).find('img'),
+            p = $(this).find('p');
+            
+        $(this).css({'width':$(window).width(), 'background-color':'#000','margin':0, 'height': fullheight, 'overflow':'hidden' });
+        $(this).find('br').remove(); //hack to remove space
+        
+        p.css({'margin':0});
+        img.css({'height': fullheight, 'width':'auto'})
+    });
+    
     
 });
