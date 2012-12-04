227183f8652ea643d06fb75a2a82ab2bc10b8949
Katarina <katarina@katarina-VGN-CR35S-L.(none)>
2012-12-04 12:20:40 +0100

    indexpage carousel fixed

--- vamlingbolaget/frontpage/views.py
+++ vamlingbolaget/frontpage/views.py
@@ -2,18 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from models import Frontpage
-from gallery.models import Gallery
+from gallery.models import Gallery, Image
 from blog.models import Post
 
 def first_page(request):
     frontpage, created = Frontpage.objects.get_or_create(pk=1)
-    #gallery,created = Gallery.objects.get_or_create(status = 'I')
-    #images = gallery.image_set.all()
-    news = Post.objects.all()
-    #print request
+    gallery = Gallery.objects.filter(status__display_on_index_page = True)
+    images = Image.objects.filter(gallery=gallery)
 
-    return render_to_response('frontpage/first_page1.html',
+    return render_to_response('frontpage/first_page.html',
         {'frontpage': frontpage,
-         #'images': images,
-         'news': news,},
+         'gallery' : gallery,
+         'images': images,
+        },
         context_instance=RequestContext(request))
