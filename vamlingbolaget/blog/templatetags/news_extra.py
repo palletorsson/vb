0f739cd7c752edf046af66c2a246f8d5bc5f4440
palle torsson <palle.torsson@gmail.com>
2012-12-14 11:33:51 +0100

    more translations

--- vamlingbolaget/blog/templatetags/news_extra.py
+++ vamlingbolaget/blog/templatetags/news_extra.py
@@ -1,12 +1,12 @@
 from django import template
 from django.templatetags.static import register
-from blog.models import New
+from blog.models import Post
 
 register = template.Library()
 
 #@register.inclusion_tag('base.html')
 def show_news():
-    news = New.objects.filter(active=True).order_by('-publish_at')[:2]
+    news = Post.objects.filter(active=True, blog=2).order_by('-publish_at')[:2]
 
     return {'news': news}
 
