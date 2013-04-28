7d1706000742f799d27bf1b4d477309344d2eda3
palle torsson <palle.torsson@gmail.com>
2013-04-28 14:04:27 +0200

    removing check for postalcode

--- vamlingbolaget/checkout/forms.py
+++ vamlingbolaget/checkout/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from models import Checkout
 
 class CheckoutForm(forms.ModelForm):
-    postcode  = forms.IntegerField(max_value=99999, help_text='max 5')
+
 
 
     def __init__(self, *args, **kwargs):
