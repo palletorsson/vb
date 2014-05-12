d3f7b554eec85c738f2ec3419a8bd9bbf8a1cc25
palle torsson <palle.torsson@gmail.com>
2014-05-12 14:30:08 +0200

    ordering sizes

--- vamlingbolaget/products/views.py
+++ vamlingbolaget/products/views.py
@@ -61,21 +61,19 @@ def detail(request, pk):
         color_id = product.color.order
         pattern_id = product.pattern.order
         qualities = Quality.objects.filter(active = True)
-        print  product.article.quality.order
         types = Type.objects.filter(active = True)
         colors = Color.objects.filter(active=True, quality = product.article.quality)
         patterns = Pattern.objects.filter(active=True, quality = product.article.quality)
 
         if (product.article.quality.order == 13):
-            sizes = Size.objects.filter(quality__pk = 1)
+            sizes = Size.objects.filter(quality__pk = 1).order_by('-pk')
         else:
-            sizes = Size.objects.filter(quality=product.article.quality)
+            sizes = Size.objects.filter(quality=product.article.quality).order_by('-pk')
 
         if (product.article.quality.order == 5) :
             colorsandpattern = PatternAndColor.objects.filter(active=True, quality__slug ='silkestrika')
         else:
             colorsandpattern = PatternAndColor.objects.filter(active=True, quality=product.article.quality)
-
 
     except:
         raise Http404
