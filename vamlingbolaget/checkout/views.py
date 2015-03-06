609f6927f5c7ca22b4f2cc307f063ffae99100d0
palle torsson <palle.torsson@gmail.com>
2015-03-06 16:43:51 +0100

    decommentaed email

--- vamlingbolaget/checkout/views.py
+++ vamlingbolaget/checkout/views.py
@@ -134,7 +134,7 @@ def checkout(request):
                 new_order.order = msg
                 new_order.save()
                 to = [request.POST['email'], 'info@vamlingbolaget.com']
-                #mail.send_mail('Din order med Vamlingbolaget: ',u'%s' %msg, 'vamlingbolagetorder@gmail.com', to,  fail_silently=False)
+                mail.send_mail('Din order med Vamlingbolaget: ',u'%s' %msg, 'vamlingbolagetorder@gmail.com', to,  fail_silently=False)
                 return HttpResponseRedirect('thanks/')
 
             if (paymentmethod == 'C'):
