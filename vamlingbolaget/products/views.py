3ced3bbc4b4e6afa55ff16b2bc2e054b3ea44e61
palle torsson <palle.torsson@gmail.com>
2016-06-05 10:54:43 +0200

    removing rea

--- vamlingbolaget/products/views.py
+++ vamlingbolaget/products/views.py
@@ -47,7 +47,7 @@ def reaindex(request):
     types = Category.objects.filter(active=True) 
     atypes = Type.objects.filter(order__lte=5, active=True)
 
-    rea = "true"
+    rea = "false"
     sizes = SIZES
 
     return render_to_response('variation/reaindex.html',
