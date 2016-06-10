626d6bccb27b339e9d4aaf1638f08b12e35167da
palle torsson <palle.torsson@gmail.com>
2016-06-10 11:41:00 +0200

    adding klarna urls

--- vamlingbolaget/checkout/urls.py
+++ vamlingbolaget/checkout/urls.py
@@ -15,6 +15,10 @@ urlpatterns = patterns('checkout.views',
     url(r'seekorders/(?P<key>[a-zA-Z0-9_.-]+)/$', 'readOrders'),
     url(r'consumorder/(?P<order_id>[a-zA-Z0-9_.-]+)/(?P<force>[a-zA-Z0-9_.-]+)/$', 'consumOrder'),
     url(r'consumordercheck/(?P<order_id>[a-zA-Z0-9_.-]+)/$', 'getOrderbyOrderNumerAndCheck'),
+    url(r'klhanks/$', 'klarna_thanks'),
+    url(r'klarnatestid/$', 'testconfirmklarnahtml'),
+    url(r'klar_push/$', 'klarna_push'),
+    url(r'klarna/$', 'checkout'),
 
 )
 
