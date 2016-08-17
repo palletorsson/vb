6c6cf5bc4dbc1a88587d2fdca9e8a3826049dab9
palle torsson <palle.torsson@gmail.com>
2016-08-17 14:23:51 +0200

    adding full variation views and urls

--- vamlingbolaget/products/views.py
+++ vamlingbolaget/products/views.py
@@ -1,16 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response 
 from django.template import RequestContext
-from models import *
+from products.models import *
 from blog.models import Post
 from gallery.models import *
 from django.http import Http404
-from fortnox.fortnox import get_headers, get_articles, get_article
-
-
+from fortnox.fortnox import get_headers, get_articles, get_article, create_article, update_article
+from fortnox.local_fortnox import get_vb_headers
 import json
-import requests
-import httplib
+from django.core import serializers
+from collections import Counter
+import csv
+import os
+from django.conf import settings
 
 
 def first_page(request):
@@ -38,6 +40,32 @@ def index(request):
 
 SIZES = ('XS', 'S', 'M', 'L', 'XL','XXL', )
 
+def fullindex(request):
+    full_variation = FullVariation.objects.filter(active=True).order_by('-order')
+    qualities = Quality.objects.filter(active=True)
+    types = Category.objects.filter(active=True)
+  
+    return render_to_response('variation/fullindex.html',
+                             {'products': full_variation,
+                              'qualities': qualities,
+                              'types': types,
+                              },
+                             context_instance=RequestContext(request))
+
+def fullexport(request, what):
+    full_variation = FullVariation.objects.filter(active=True).order_by('-order')
+
+    for var in full_variation:
+        print var.variation.article.sku_number, "|", var.variation.color, "|", var.variation.pattern, "|", var.size, "|", var.stock, "|"
+        print str(var.variation.article.sku_number) + "_" + str(var.variation.color.order) + "_" + str(var.variation.pattern.order) + "_" + str(var.size)  
+
+    return render_to_response('variation/fullindex.html',
+                             {'products': full_variation,
+
+                              },
+                             context_instance=RequestContext(request))
+
+
 
 def reaindex(request):
 
@@ -59,6 +87,16 @@ def reaindex(request):
                               'sizes': sizes, 
                               },
                              context_instance=RequestContext(request))
+def articleindex(request):
+
+    articles = Article.objects.filter(active='A').order_by('name')
+
+    return render_to_response('variation/articleindex.html',
+                             {'articles': articles,
+
+                              },
+                             context_instance=RequestContext(request))
+
 
 def rea_by_size(request, key):
     products = ReaArticle.objects.filter(size__name = key, status='A').order_by('article__name')
@@ -116,6 +154,16 @@ def by_quality(request, key):
          'types': types,},
         context_instance=RequestContext(request))
 
+def product_api(request, key):
+   
+    product = Variation.objects.get(pk=key)
+    print product
+
+
+    resp_d = {'product': product.article.name}
+    return HttpResponse(json.dumps(resp_d), content_type="application/json")
+
+
 
 def detail(request, pk):
     try:
@@ -123,7 +171,6 @@ def detail(request, pk):
 
         products = Variation.objects.filter(article=product.article).order_by('color')
 
-        images = Image.objects.filter(variation__pk=pk)
         color_id = product.color.order
         pattern_id = product.pattern.order
         qualities = Quality.objects.filter(active = True)
@@ -143,7 +190,13 @@ def detail(request, pk):
         
 
     except:
-        raise Http404
+        raise Http404 
+
+    try:        
+        images = Image.objects.filter(variation__pk=pk)
+    except:
+        raise Http404 
+        
 
     return render_to_response('variation/detail.html',
                    {
@@ -171,6 +224,77 @@ def readetail(request, pk):
 
     return render_to_response('variation/readetail.html',
                    {'product': reaArticle,
+                   },
+                   context_instance=RequestContext(request)
+                    )
+
+def fulldetail(request, pk):
+    try:
+        full_variation = FullVariation.objects.get(pk=pk)
+        images = Image.objects.filter(variation__pk=full_variation.variation.pk)
+        sizes = SIZES
+    except:
+        raise Http404
+
+    full_variations_article = FullVariation.objects.filter(variation__article=full_variation.variation.article)    
+    full_variations = FullVariation.objects.filter(variation=full_variation.variation)   
+
+    init_sizes = ['XS','S','M','L', 'XL', 'XXL']
+
+    size_list = []                           
+
+    colorpattern_list = []
+
+
+    for full_var in full_variations_article: 
+
+        color_pattern_str = str(full_var.variation.color.order)+"f_"+str(full_var.variation.pattern.order)+"m"
+        colorpattern_list.append(color_pattern_str)
+    
+
+    for full_var in full_variations: 
+        size_list.append(full_var.size)
+
+    variation_sizes = Counter(size_list)
+
+    colorpattern_list = Counter(colorpattern_list)
+
+    variation_sizes = list(variation_sizes)
+    colorpattern_list = list(colorpattern_list)
+    path_dir = settings.ROOT_DIR
+    filename = str(full_variation.variation.article.sku_number) + "_" + str(full_variation.variation.color.order) + "_" + str(full_variation.variation.pattern.order) 
+
+    images = [] 
+
+    for x in range(0, 3):
+        image = path_dir + "/media/variations/"+ filename +"_" + str(x+1) + ".jpg"        
+        file_exist = os.path.isfile(image) 
+        file = "/media/variations/"+ filename +"_" + str(x+1) + ".jpg"        
+        if file_exist: 
+          images.append(file)
+  
+    return render_to_response('variation/fulldetail.html',
+                   {'product': full_variation,
+                   'images': images,
+                   'sizes': variation_sizes,
+                   'colorsandpatterns': colorpattern_list,
+                   'init_sizes': init_sizes, 
+                   'full_variations': full_variations
+                   },
+                   context_instance=RequestContext(request)
+                    )
+
+
+def artdetail(request, pk):
+    try:
+        article_ = Article.objects.get(pk=pk)
+    except:
+        raise Http404
+
+    products = Variation.objects.filter(article=article)
+    return render_to_response('variation/artdetail.html',
+                   {'article': article_,
+                   'products': products,
                    },
                    context_instance=RequestContext(request)
                     )
@@ -226,6 +350,7 @@ def quality(request, name):
 def category(request, name):
     pass
 
+
 def allArticles(request): 
     headers = get_headers()
     articles = get_articles(headers)
@@ -234,11 +359,13 @@ def allArticles(request):
         'articles': articles
     }, context_instance=RequestContext(request))
 
+def checkArticles(request): 
+    headers = get_headers()  
+    articles = get_articles(headers)
 
 # to show all articels
 def allArt(request):
     articles = Article.objects.filter(active = True).order_by('name')
-    print articles
 
     headers = get_headers()
     check_art = []
@@ -273,4 +400,96 @@ def allreaArt(request):
         'articles': reaarticles, 
 
     }, context_instance=RequestContext(request))
+
+
+def articleList(request):
+    articles = Article.objects.filter(active = True).order_by('name')
+    qualities = Quality.objects.filter(active=True)
+    types = Category.objects.filter(active=True)
+    return render_to_response('articles/index.html',
+                             {'articles': articles,
+                              'qualities': qualities,
+                              'types': types,
+                              },
+                             context_instance=RequestContext(request))
+
+def articlesCsv(request): 
+    articles = Article.objects.filter(active = True).order_by('name')
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+
+    # The data is hard-coded here, but you could load it from a database or
+    # some other source.
+    csv_data = []
+
+    for art in articles:
+        temp = (art.name.encode('iso-8859-1'), art.sku_number)
+        csv_data.append(temp)
+
+
+
+    t = loader.get_template('articles/csv_temp.txt')
+    c = Context({
+        'data': csv_data,
+    })
+    response.write(t.render(c))
+    return response
+
+
+
+def articlesTranferToFortnox(request): 
+    articles = Article.objects.filter(active = True).order_by('name')
+    headers = get_headers()
+    
+    for art in articles: 
+        try: 
+            sku_num = int(art.sku_number)
+            sku_num = str(sku_num)
+        except: 
+            sku_num = str(1) 
+
+        res = get_article(headers, int(sku_num)) 
+        data = json.dumps({
+            "Article": {
+                "Description": art.name,
+                "ArticleNumber": int(sku_num), 
+                "WebshopArticle": True, 
+            }
+        })
+        
+        if len(res) > 100: 
+            exist = True
+            updated = update_article(int(sku_num), data, headers)
+            print updated 
+        else: 
+            exist = False
+            created = create_article(int(sku_num), data, headers)
+            print created   
+
+    return HttpResponse(status=200)    
+         
+def articleUpdateStock(request, sku_num, stock): 
+    sku_number = sku_num
+    headers = get_headers()
+    article = Article.objects.get(sku_number = sku_number)
+    
+    
+    res = get_article(headers, sku_number) 
+    data = json.dumps({
+        "Article": {
+            "Description": article.name,
+            "ArticleNumber": 1401, 
+            "QuantityInStock": stock, 
+        }
+    })
+        
+    if len(res) > 100: 
+        exist = True
+        updated = update_article(sku_number, data, headers)
+        print updated 
+
+    return HttpResponse(status=200)    
+
+
+# article_sku, color, pattern, 1223_L02_MAH_01 1325_001_001_XL
 
