963c9aa6700771fd8e366a9a47c8cbc45ae49d01
palle torsson <palle.torsson@gmail.com>
2016-09-28 13:08:53 +0200

    template for page preview of frontpages

--- vamlingbolaget/frontpage/views.py
+++ vamlingbolaget/frontpage/views.py
@@ -33,7 +33,7 @@ def first_page_opt(request, opt):
     features = FrontpageExtended.objects.filter(status='A', theme__title=opt).order_by('order')
     theme1 = FrontpageTheme.objects.filter(status='A').order_by('order')[:1]
 
-    return render_to_response('frontpage/first_page.html',
+    return render_to_response('frontpage/first_page_now.html',
         {'frontpage': frontpage,
          'gallery' : gallery,
          'images': images,
