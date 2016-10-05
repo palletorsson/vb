9b84903373bd8e464e77a2ce7535a1a65617ca11
palle torsson <palle.torsson@gmail.com>
2016-10-05 21:54:23 +0200

    variations can be used on frontpage

--- vamlingbolaget/frontpage/views.py
+++ vamlingbolaget/frontpage/views.py
@@ -4,6 +4,7 @@ from django.template import RequestContext
 from models import Frontpage, FrontpageTheme, FrontpageExtended
 from gallery.models import Gallery, Image
 from blog.models import New
+from products.models import Variation, Article, Pattern, Color
 
 def first_page(request):
     frontpage, created = Frontpage.objects.get_or_create(pk=1)
@@ -32,7 +33,19 @@ def first_page_opt(request, opt):
     news = New.objects.filter(active=True).order_by('-publish_at')[:2]
     features = FrontpageExtended.objects.filter(status='A', theme__title=opt).order_by('order')
     theme1 = FrontpageTheme.objects.filter(status='A').order_by('order')[:1]
-
+    print features
+    for feature in features: 
+        try: 
+            heading_split = feature.heading.split("_")
+            if int(heading_split[0]) > 1: 
+                    article = Article.objects.get(sku_number=heading_split[0])
+                    pattern = Pattern.objects.get(order=int(heading_split[2]))
+                    color = Color.objects.get(order=heading_split[1])
+                    variation = Variation.objects.get(article=article, pattern=pattern, color=color)
+                    feature.heading = variation
+                
+        except: 
+            pass
     return render_to_response('frontpage/first_page_now.html',
         {'frontpage': frontpage,
          'gallery' : gallery,
