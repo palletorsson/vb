a3453c31c1fbcadfec67b5b44a17a2e85ba51d00
palle torsson <palle.torsson@gmail.com>
2016-10-13 12:50:05 +0200

    adding manchster to csv read

--- vamlingbolaget/products/views.py
+++ vamlingbolaget/products/views.py
@@ -673,11 +673,27 @@ def articleUpdateStock(request, sku_num, stock):
 
 # import or update fullvaration from csv                     
 def fromCsvToDjango(article, pattern, color, size, stock):
-    print article, pattern, color, size, stock
-    variation, created_variation = Variation.objects.get_or_create(article=article, pattern=pattern, color=color)
-    print "--"
-    fullvariation, created_fullvariation = FullVariation.objects.get_or_create(variation=variation, size=size, stock=stock)
-    print "---"
+    not_created = 0 
+    print "je", article, pattern, color, size, stock
+    try: 
+        variation, created_variation = Variation.objects.get_or_create(article=article, pattern=pattern, color=color)
+        print "--"
+        fullvariation, created_fullvariation = FullVariation.objects.get_or_create(variation=variation, size=size, stock=stock)
+        print "---"
+    except: 
+        not_created = 1
+
+    if not_created == 1: 
+        try:
+            obj = Variation.objects.get(article=article, pattern=pattern, color=color)
+            print "*", obj
+        except Variation.DoesNotExist:
+            obj = Variation(article=article, pattern=pattern, color=color)
+            print "**", obj 
+            obj.save()
+            fullvariation, created_fullvariation = FullVariation.objects.get_or_create(variation=variation, size=size, stock=stock)
+
+
     # if fullvariation exist only update the fullvaration with stockvalue
     if created_fullvariation == False: 
         fullvariation.stock = stock
