11eb0c85c7bae6b1d388c4b86a495880d6485f24
palle torsson <palle.torsson@gmail.com>
2017-03-29 13:53:28 +0200

    adding ondemand cost to article

---
+++ vamlingbolaget/products/local_fortnox.py
@@ -0,0 +1,6 @@
+# header helper function 
+def get_headers(): 
+    return {"Client-Secret":"8yBYUocg4P",
+           "Content-Type":"application/json",
+           "Access-Token":"acec4405-41b4-434d-aef2-23f01c36048a",
+           "Accept":"application/json"}
