68e9b141c55719b1cd3c3a6fcd9d770c30523d08
palle torsson <palle.torsson@gmail.com>
2017-03-29 14:01:31 +0200

    removing files from git

--- vamlingbolaget/products/local_fortnox.py
+++
@@ -1,6 +0,0 @@
-# header helper function 
-def get_headers(): 
-    return {"Client-Secret":"8yBYUocg4P",
-           "Content-Type":"application/json",
-           "Access-Token":"acec4405-41b4-434d-aef2-23f01c36048a",
-           "Accept":"application/json"}
