d76e05f9fa1dea259ab7d5a3b07b56fde8588342
palle torsson <palle.torsson@gmail.com>
2017-09-23 18:17:21 +0200

    react for rea

--- vamlingbolaget/products/views.py
+++ vamlingbolaget/products/views.py
@@ -148,6 +148,17 @@ def reaindex(request):
                               },
                              context_instance=RequestContext(request))
 
+def reareact(request):
+    rea = "true"
+    sizes = SIZES
+    return render_to_response('variation/reatest.html',
+                             {
+                              'rea': rea, 
+                              'sizes': sizes, 
+                              },
+                             context_instance=RequestContext(request))
+
+
 def jsonReaindex(request):
     products = ReaArticle.objects.filter(status='A').order_by('-article__name')
     allproducts = []
