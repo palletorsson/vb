40b87439fbfe7c61ce77391b94d9e7fc14dfc972
plix <palle.torsson@gmail.com>
2017-10-13 14:26:33 +0200

    scroll js

--- vamlingbolaget/theme/static/js/vb.js
+++ vamlingbolaget/theme/static/js/vb.js
@@ -979,18 +979,21 @@ function scroll_init() {
         var header = document.querySelector("header");
         if (distanceY > shrinkOn) {
             $('.menu_container').animate({ height: "50px" }, 1000 ).addClass('shriked')
-            $('.navbar-brand').hide()
-            $('.midhead').hide()
-            $('.main_nav').animate({ top: "1px" }, 1000 )
+            
+            $('.main_nav').animate({ top: "-11px" }, 1000, function {
+                $('.navbar-brand').hide()
+                $('.midhead').hide()
+            });
 
             
         }  else {
            if ($('.menu_container').hasClass('shriked')) {
               $('.menu_container').removeClass('shriked')
               $('.menu_container').animate({ height: "160px" }, 1000 )
-              $('.navbar-brand').show()
-              $('.midhead').show()
-              $('.main_nav').animate({ top: "80px" }, 1000 )
+              $('.main_nav').animate({ top: "80px" }, 1000 , function {
+                $('.navbar-brand').show()
+                $('.midhead').show()
+            });
               console.log(distanceY, shrinkOn)
            }
         }
