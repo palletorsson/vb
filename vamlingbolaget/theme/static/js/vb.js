5be25b769bdfb36ef68c87a23ed69423a76ee619
plix <palle.torsson@gmail.com>
2017-10-18 11:52:56 +0200

    strat for adding class

--- vamlingbolaget/theme/static/js/vb.js
+++ vamlingbolaget/theme/static/js/vb.js
@@ -120,7 +120,7 @@ if($('#nosize')[0]) {
     var some_size = Math.floor(allsizes.length/2)-1; 
     if (some_size < 0) { some_size = 0; }
     console.log(allsizes[some_size].id)
-    this[some_size].addClass( "size_active active" ); 
+    $(this)[some_size].addClass( "size_active active" ); 
     var thetargetsize = $("#"+allsizes[some_size].id); 
 
     console.log(thetargetsize.length, thetargetsize); 
