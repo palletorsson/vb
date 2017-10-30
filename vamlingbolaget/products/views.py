9f286a2d4b5437524ecb1f9a25ea0e5958b9759d
plix <palle.torsson@gmail.com>
2017-10-30 17:46:55 +0100

    fixing template

--- vamlingbolaget/products/views.py
+++ vamlingbolaget/products/views.py
@@ -352,8 +352,8 @@ def articleDetail(request, pk):
         pattern_id = product.pattern.order
         qualities = Quality.objects.filter(active = True)
         types = Type.objects.filter(active = True)
-        colors = Color.objects.filter(active=True, quality = product.article.quality)
-        patterns = Pattern.objects.filter(active=True, quality = product.article.quality)
+        colors = Color.objects.filter(active=True, quality = product.quality)
+        patterns = Pattern.objects.filter(active=True, quality = product.quality)
         print "----"
         if (product.quality.order == 13):
             sizes = Size.objects.filter(quality__pk = 1).order_by('-pk')
