533ddeb66ef967e53b6eac6facd16d124aafbc9c
plix <palle.torsson@gmail.com>
2017-12-14 15:10:32 +0100

    adding

--- vamlingbolaget/products/views.py
+++ vamlingbolaget/products/views.py
@@ -80,7 +80,7 @@ def cutondemandApi(request, category):
           "category": sellart.variation.article.category.name, 
           "description": sellart.variation.article.description,
           "quality": sellart.variation.article.quality.name,
-          "cod_cost": sellart.variation.article.ondemand_cost
+          "cod_cost": sellart.variation.article.ondemand_cost,
           "pattern": sellart.variation.pattern,  
           "color": sellart.variation.color,  
           "size": sellart.size, 
