5abb6c54e18a75ff37905c9bd550a9a4b64f3f94
plix <palle.torsson@gmail.com>
2018-01-24 11:19:32 +0100

    minor

--- vamlingbolaget/frontpage/urls.py
+++ vamlingbolaget/frontpage/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('frontpage.views',
     url(r'index/(?P<opt>[a-zA-Z0-9_.-]+)/$', 'first_page_opt'),
-    url(r'test/^$', 'first_page_b'),
+    url(r'test/$', 'first_page_b'),
     url(r'^$', 'first_page'),
 )
 
