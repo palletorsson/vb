e7c4a089fc05ed528bfccaa87c51d788eb29afee
plix <palle.torsson@gmail.com>
2018-01-31 11:20:32 +0100

    test frontpage

--- vamlingbolaget/frontpage/urls.py
+++ vamlingbolaget/frontpage/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('frontpage.views',
+    url(r'test/$', 'first_page_b'),
     url(r'index/(?P<opt>[a-zA-Z0-9_.-]+)/$', 'first_page_opt'),
     url(r'^$', 'first_page'),
 )
