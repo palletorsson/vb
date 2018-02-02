0e5f47669e1de7f65435f50e187b569b93fc3456
plix <palle.torsson@gmail.com>
2018-02-02 14:04:33 +0100

    link

--- vamlingbolaget/checkout/urls.py
+++ vamlingbolaget/checkout/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('checkout.views',
     url(r'^$', 'checkout'),
-    url(r'_/$', 'checkout_b'),
+    url(r'test/', 'checkout_b'),
     url(r'thanks/', 'thanks'),
     url(r'success/', 'success'),
     url(r'cancel/', 'cancel'),
