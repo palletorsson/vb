55d77f4c0eb51fb09ce10b732b7cfc30af4bb492
plix <palle.torsson@gmail.com>
2018-02-03 11:03:49 +0100

    text-info

--- vamlingbolaget/theme/static/js/vb.js
+++ vamlingbolaget/theme/static/js/vb.js
@@ -328,11 +328,11 @@ $("#addtofullcart").off('click').on({
                       height:'200px'
                     });
 
-                    $("#updatecart").html( '<div class="card"> <div class="card-body"> <h6 class="card-title">'+ widgetTextstart+ ' '+_.article +' </h6>' +
-                    ' <p class="card-text">'+ widget_size +' </p>' +
+                    $("#updatecart").html( '<div class="card border-info mb-3" style="max-width: 18rem;"> <div class="card-header"> '+ widgetTextstart+ ' '+_.article +' </div>' +
+                    ' <div class="card-body text-info"> <p class="card-text">'+ widget_size +' </p>' +
                     ' <p class="card-text">'+ coltext +', '+ pattext +' </p>' +
                    
-                    ' </div></div>').fadeIn();
+                    ' </div></div></div>').fadeIn();
 
                 $("#updatecart").delay(6000).fadeOut(3000).animate({
                       height:'0px'
