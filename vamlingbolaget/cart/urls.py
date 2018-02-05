2ee6bd41e3044edf8bba31227260197bfa722be3
plix <palle.torsson@gmail.com>
2018-02-05 22:15:42 +0100

    cart show b

--- vamlingbolaget/cart/urls.py
+++ vamlingbolaget/cart/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('cart.views',
     url(r'^s_how/$', 'showcart_b', name='showcart_b'),
-    url(r'^show/$', 'showcart', name='showcart'),
+    url(r'^show/$', 'showcart', name='showcart_b'),
     url(r'^showbyid/(?P<session_id>[a-zA-Z0-9_.-]+)/$', 'showcartBySessionId', name='showcart'),
     url(r'^addtocart/$', 'addtocart'),
     url(r'^remove/(?P<pk>[a-zA-Z0-9_.-]+)/(?P<type>[a-zA-Z0-9_.-]+)/$', 'removefromcart',  name='removefromcart'),
