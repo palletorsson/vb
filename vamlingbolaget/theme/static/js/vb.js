3e36c95c3422539120bdd44e9a806655cd04e2ef
plix <palle.torsson@gmail.com>
2018-02-14 17:32:45 +0100

    testing

--- vamlingbolaget/theme/static/js/vb.js
+++ vamlingbolaget/theme/static/js/vb.js
@@ -631,7 +631,7 @@ $('.overlay').each(function(){
 
 
 var path = location.pathname
-var link = $('a[href="'+window.location.pathname+'"] span')
+var link = $('a[href="'+window.location.pathname+'"]')
 link.addClass('active-link');
 
 $('body input[type=text]').addClass('form-control');
