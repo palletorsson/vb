8746435fa9af16f05d3e8530e36daa8e19f42f87
plix <palle.torsson@gmail.com>
2019-04-18 09:13:04 +0200

    Update views.py

--- vamlingbolaget/products/views.py
+++ vamlingbolaget/products/views.py
@@ -82,11 +82,12 @@ def cutondemandApi(request, category):
         if (category == 'all'):
             products = FullVariation.objects.filter(active=True, size=3840).order_by('order')
         else:
-            products = FullVariation.objects.filter(active=True, size=3840, variation__article__category__slug=category).order_by('order')
+            products = FullVariation.objects.filter(active=True, size=3840, variation__article__category__slug='silkestrika').order_by('order')
 
             variations = Variation.objects.filter(Q(article__category__slug='barn') | Q(article__category__slug='accessoarer') |  Q(article__category__slug='piece-goods'), order__lte=100, active=True).order_by('article__type')
     print "cut on demand api"
     print products
+    print variations
     articles = Article.objects.filter(quality__slug ='silkestrika', active=True).order_by('type')
 
     colorsandpatterns = PatternAndColor.objects.filter(active=True, quality__slug ='silkestrika')
