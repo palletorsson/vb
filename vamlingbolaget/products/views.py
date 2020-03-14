ff3c6d3f6dfb25124168781d30488e6128510008
plix <palle.torsson@gmail.com>
2020-03-14 16:52:36 +0100

    Update views.py

--- vamlingbolaget/products/views.py
+++ vamlingbolaget/products/views.py
@@ -741,25 +741,23 @@ def cutondemandApiSingle(request, sku_number):
 #    return HttpResponse(resp, content_type="application/json")
 
 def cutondemandApiStickeri(request):
-	allpossiblities = {}
-	try:
-		articles = Article.objects.filter(quality__slug='stickat-100-ekologisk-ull', active=True).order_by('type')
-		colorsandpatterns = PatternAndColor.objects.filter(active=True, quality__slug='stickat-100-ekologisk-ull')
-		active_sizes = SIZES
-		allpossiblities["sizes"] = active_sizes
-		allpossiblities["colorspatterns"] = []
-		allpossiblities["articles"] = []
-		for csps in colorsandpatterns:
-	    	allpossiblities["colorspatterns"].append({
-				"color_num": csps.color.order,
-				"color_name": csps.color.name,
-				"pattern_num": csps.pattern.order,
-				"pattern_name": csps.pattern.name,
-				"quality_name": csps.quality.name,
-				"quality_num": csps.quality.order
-	          })
-    except:
-        print "stickeri error"
+	jsonresp = {}
+	articles = Article.objects.filter(quality__slug='stickat-100-ekologisk-ull', active=True).order_by('type')
+	colorsandpatterns = PatternAndColor.objects.filter(active=True, quality__slug='stickat-100-ekologisk-ull')
+	active_sizes = SIZES
+	jsonresp["sizes"] = active_sizes
+	jsonresp["colorspatterns"] = []
+	jsonresp["articles"] = []
+	for csps in colorsandpatterns:
+		jsonresp["colorspatterns"].append({
+			"color_num": csps.color.order,
+			"color_name": csps.color.name,
+			"pattern_num": csps.pattern.order,
+			"pattern_name": csps.pattern.name,
+			"quality_name": csps.quality.name,
+			"quality_num": csps.quality.order
+          })
+
     html = "<html><body>hello</body></html>"
     return HttpResponse(html)
 
